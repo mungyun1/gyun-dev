f1fe6dcb761807c6a23e57b045e46348aeaf691e
mungyun <mungyun1234@naver.com>
2025-08-03 22:00:21 +0900

    Feat: 배포 시, 콘솔문 제거 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev, isServer }) => {
+    // 프로덕션 빌드에서만 콘솔문 제거
+    if (!dev) {
+      const TerserPlugin = require("terser-webpack-plugin");
+      config.optimization.minimizer = config.optimization.minimizer || [];
+      config.optimization.minimizer.push(
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, // console.log, console.info, console.debug 제거
+              drop_debugger: true, // debugger 제거
+            },
+          },
+        })
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
